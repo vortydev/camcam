b1b61882cd948c2ff79c9a4982ac02f6cb1f2ea6
vortydev <etienne.dmenard@gmail.com>
2022-04-07 23:16:53 -0400

    getting started!

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 ########################################################
-# Fichier :         moniteur.py
-# Description :     Classe de l'objet Moniteur.
+# Fichier :         client.py
+# Description :     Programme du Client Cam-Cam.
 # Auteurs :         Étienne Ménard, Isabelle Rioux
 # Création :        2022/04/13
 # Modification :    2022/04/13
 ########################################################
+
+# import paho.mqtt.client as mqtt
